4eb275f9a6e241f4e01daf7031c4f04c0b400d2d
Wei Kang <wei.kang8775@gmail.com>
2020-12-04 13:04:15 -0700

    Add register frame

---
+++ ENSF480FinalProject/src/view/RegisterFrame.java
@@ -0,0 +1,20 @@
+package view;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import model.shared.Address;
+
+public class RegisterFrame extends JFrame {
+
+}
